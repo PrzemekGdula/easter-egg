68a1fc6d5d17819273c81af4b16595cba23bfa5f
PrzemekGdula <przemo.g12@wp.pl>
2019-04-23 07:53:19 +0200

    egg finished

--- src/EggNotPainted.js
+++ src/EggNotPainted.js
@@ -3,15 +3,27 @@ import egg from './egg.svg'
 
 
 
+const styles = {
+    divContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:'center'
+    },
+    img: {
+        width: '200px'
+    }
+}
+
 const EggNotPainted = (props) => {
     return (
         <div
+        style={styles.divContainer}
         >
-            <h1
-            >
+            <h1>
                 Kliknij aby pomalować jajko!
 			</h1>
             <img
+            style={styles.img}
                 src={egg}
                 alt={'Not painted easter egg'}
                 onClick={() => props.paintEgg(true)}
--- src/EggPainted.js
+++ src/EggPainted.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import eggPainted from './egg-painted.svg'
 
+const styles = {
+    divContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:'center'
+    },
+    img: {
+        width: '200px'
+    }
+}
+
 const EggPainted = (props) => {
     return(
-        <div>
+        <div
+        style={styles.divContainer}
+        >
 <h1>
     Wesołych Świąt
 </h1>
 <img
+style={styles.img}
 src = {eggPainted}
 alt = {'Painted easter egg'}
 />
